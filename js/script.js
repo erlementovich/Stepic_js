f95e189c02715cdc19152976cbe9fb89b3a83355
Artem <Erlementovich@yandex.ru>
2018-11-14 16:15:42 +0300

    Селекторы Class,Tag,Id вывод на страницу

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 console.log('Ты на ' +window.location);
+
+const byTag = document.getElementsByTagName('div');
+const byClass = document.getElementsByClassName('myClass');
+const byID = document.getElementById('myId');
+
+console.log(byTag,byClass,byID);
+
+const allBySelector = document.querySelectorAll('.coolClass.mine');
+const firstBySelector = document.querySelector('div');
+console.log(allBySelector, firstBySelector);
+
+const quest = prompt('Что хотите написать на странице?')
+
+byID.innerHTML = quest;
